c3e9a329f0c63d2386def7f2691251369b547073
Alkxyly Samyr <alkxyly@gmail.com>
2026-05-26 21:44:52 -0300

    feat: corrige execucao mesmo quando nao ha tetes implementados

--- karma.conf.js
+++ karma.conf.js
@@ -44,6 +44,7 @@ module.exports = function (config) {
       }
     },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    failOnEmptyTestSuite: false
   });
 };
